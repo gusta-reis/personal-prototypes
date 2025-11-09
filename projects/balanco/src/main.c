7ff678f06148a4437c65fcda6325609e60a45080
Gustavo Reis <gustavophelipe_reis@hotmail.com>
2025-11-09 10:45:28 -0400

    moded: modulation of motor by potenciometer

--- projects/balanco/src/main.c
+++ projects/balanco/src/main.c
@@ -16,7 +16,18 @@ LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 
 #define ADC_CHANNEL_NODE DT_PATH(zephyr_user)
 static const struct adc_dt_spec adc_channel = ADC_DT_SPEC_GET_BY_IDX(ADC_CHANNEL_NODE, 0);
+
+// Limites do ADC (0-4095) e limites reais do sensor (130-2900)
+#define ADC_INPUT_MIN 130
+#define ADC_INPUT_MAX 2370
+#define ADC_INPUT_RANGE (ADC_INPUT_MAX - ADC_INPUT_MIN)
+
+#define REMAP_MAX 6000
+#define REMAP_MIN 3000
+#define REMAP_SPN (REMAP_MAX - REMAP_MIN)
+
 static int16_t sample_buffer;
+static int32_t remap = REMAP_MIN;
 
 void adc_read_thread(void *p1, void *p2, void *p3)
 {
@@ -53,10 +64,28 @@ void adc_read_thread(void *p1, void *p2, void *p3)
     while (1) {
         int err = adc_read(adc_channel.dev, &sequence);
         if (err) LOG_ERR("ADC read error: %d", err);
-        else LOG_INF("Leitura ADC: %d (bruto)", sample_buffer);
-        k_msleep(1000);
+        else {
+            int32_t clamped_value = sample_buffer;
+
+            // 1. Limita (clampa) o valor lido ao intervalo útil definido
+            if (clamped_value < ADC_INPUT_MIN) {
+                clamped_value = ADC_INPUT_MIN;
+            }
+            if (clamped_value > ADC_INPUT_MAX) {
+                clamped_value = ADC_INPUT_MAX;
+            }
+
+            // 2. Remapeia o valor limitado (ex: 130-2900) para o percentual (1-100%)
+            int32_t percentage = ((clamped_value - ADC_INPUT_MIN) * 100) / ADC_INPUT_RANGE;
+            remap = REMAP_MIN + (percentage * REMAP_SPN) / 100;
+            LOG_INF("Leitura ADC: %d (bruto) -> %d%%, valor remapeado: %d", sample_buffer, percentage, remap);
+        }
+        k_msleep(100);
     }
 }
+
+
+
 
 // ======== CONFIGURAÇÃO DO MOTOR ========
 
@@ -70,7 +99,7 @@ static const struct gpio_dt_spec coil_a_minus = GPIO_DT_SPEC_GET(AMINUS, gpios);
 static const struct gpio_dt_spec coil_b_plus  = GPIO_DT_SPEC_GET(BPLUS, gpios);
 static const struct gpio_dt_spec coil_b_minus = GPIO_DT_SPEC_GET(BMINUS, gpios);
 
-#define STEP_DELAY_MS K_MSEC(4)
+//#define STEP_DELAY_MS K_USEC(2000)
 
 static inline void step_forward(void)
 {
@@ -78,25 +107,25 @@ static inline void step_forward(void)
     gpio_pin_set_dt(&coil_a_minus, 0);
     gpio_pin_set_dt(&coil_b_plus, 1);
     gpio_pin_set_dt(&coil_b_minus, 0);
-    k_sleep(STEP_DELAY_MS);
+    k_sleep(K_USEC(remap));
 
     gpio_pin_set_dt(&coil_a_plus, 0);
     gpio_pin_set_dt(&coil_a_minus, 1);
     gpio_pin_set_dt(&coil_b_plus, 1);
     gpio_pin_set_dt(&coil_b_minus, 0);
-    k_sleep(STEP_DELAY_MS);
+    k_sleep(K_USEC(remap));
 
     gpio_pin_set_dt(&coil_a_plus, 0);
     gpio_pin_set_dt(&coil_a_minus, 1);
     gpio_pin_set_dt(&coil_b_plus, 0);
     gpio_pin_set_dt(&coil_b_minus, 1);
-    k_sleep(STEP_DELAY_MS);
+    k_sleep(K_USEC(remap));
 
     gpio_pin_set_dt(&coil_a_plus, 1);
     gpio_pin_set_dt(&coil_a_minus, 0);
     gpio_pin_set_dt(&coil_b_plus, 0);
     gpio_pin_set_dt(&coil_b_minus, 1);
-    k_sleep(STEP_DELAY_MS);
+    k_sleep(K_USEC(remap));
 }
 
 static inline void step_backward(void)
@@ -105,25 +134,25 @@ static inline void step_backward(void)
     gpio_pin_set_dt(&coil_a_minus, 0);
     gpio_pin_set_dt(&coil_b_plus, 0);
     gpio_pin_set_dt(&coil_b_minus, 1);
-    k_sleep(STEP_DELAY_MS);
+    k_sleep(K_USEC(remap));
 
     gpio_pin_set_dt(&coil_a_plus, 0);
     gpio_pin_set_dt(&coil_a_minus, 1);
     gpio_pin_set_dt(&coil_b_plus, 0);
     gpio_pin_set_dt(&coil_b_minus, 1);
-    k_sleep(STEP_DELAY_MS);
+    k_sleep(K_USEC(remap));
 
     gpio_pin_set_dt(&coil_a_plus, 0);
     gpio_pin_set_dt(&coil_a_minus, 1);
     gpio_pin_set_dt(&coil_b_plus, 1);
     gpio_pin_set_dt(&coil_b_minus, 0);
-    k_sleep(STEP_DELAY_MS);
+    k_sleep(K_USEC(remap));
 
     gpio_pin_set_dt(&coil_a_plus, 1);
     gpio_pin_set_dt(&coil_a_minus, 0);
     gpio_pin_set_dt(&coil_b_plus, 1);
     gpio_pin_set_dt(&coil_b_minus, 0);
-    k_sleep(STEP_DELAY_MS);
+    k_sleep(K_USEC(remap));
 }
 
 static inline void step_off(void)
@@ -141,15 +170,16 @@ void motor_thread(void *p1, void *p2, void *p3)
     ARG_UNUSED(p3);
 
     while (1) {
-        printk("Girando no sentido horário...\n");
-        for (int i = 0; i < 1000; i++) step_forward();
-        step_off();
-        k_sleep(K_SECONDS(2));
+        //printk("Girando no sentido horário...\n");
+        //for (int i = 0; i < 1000; i++) step_forward();
+        //step_off();
+        //k_sleep(K_SECONDS(2));
 
         printk("Girando no sentido anti-horário...\n");
-        for (int i = 0; i < 1000; i++) step_backward();
-        step_off();
-        k_sleep(K_SECONDS(2));
+        //for (int i = 0; i < 1000; i++)
+        step_backward();
+        //step_off();
+        //k_sleep(K_SECONDS(2));
     }
 }
 
@@ -188,7 +218,7 @@ int main(void)
                     motor_thread, NULL, NULL, NULL, 5, 0, K_NO_WAIT);
 
     k_thread_create(&adc_data, adc_stack, K_THREAD_STACK_SIZEOF(adc_stack),
-                    adc_read_thread, NULL, NULL, NULL, 7, 0, K_SECONDS(1));
+                    adc_read_thread, NULL, NULL, NULL, 7, 0, K_MSEC(100));
     
     return 0;
 }
